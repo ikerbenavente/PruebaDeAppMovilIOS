e5c9e1d0035c6e894464a9d6d565daa29fcb1595
Iker Benavente <iker.benavente@espaciorack.com>
2026-03-24 15:53:31 +0100

    Inicial: Proyecto MAUI con contador

---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace pruebaMaui;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+        CounterBtn.Text = $"Haz hecho clic {count} veces";
+    }
+}
